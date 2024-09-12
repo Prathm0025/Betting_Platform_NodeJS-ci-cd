a60ae89f2c2886bdf4a0e455bd88c8624a3da7db
rohit <rohit@underpinservices.com>
2024-09-12 11:28:05 +0530

    feat: search event #1

--- src/store/storeRoutes.ts
+++ src/store/storeRoutes.ts
@@ -2,11 +2,26 @@ import express from "express";
 import Store from "./storeController";
 
 const storeRoutes = express.Router();
-// const store = new Store()
 
-// storeRoutes.get("/", store.getRequestCount);
-// storeRoutes.get("/sports", store.getSports);
-// storeRoutes.get("/sports/:sport/events", store.getSportEvents);
+//NOTE: for testing 
+
+// storeRoutes.get("/", Store.getRequestCount);
+// storeRoutes.get("/sports", async (req, res) => {
+//   try {
+//
+//     const sportName = req.query.sportName as string | undefined;
+//     const query = req.query.query as string | undefined;
+//     if (sportName === undefined) {
+//       return res.status(400).json({ error: "Missing sportName" });
+//     }
+//     const sports = await Store.searchEvent(sportName, query);
+//     return res.json(sports);
+//   } catch (err) {
+//     console.log(err)
+//     return res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+// storeRoutes.get("/sports/:sport/events", Store.getSportEvents);
 
 
 export default storeRoutes
