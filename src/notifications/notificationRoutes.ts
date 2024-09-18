d4cbed0f8d49b1de4731eb5b7e9c73732262a3f1
Anushaka23 <anushaka.underpinservices@gmail.com>
2024-09-18 11:14:44 +0530

    add token from header

--- src/notifications/notificationRoutes.ts
+++ src/notifications/notificationRoutes.ts
@@ -6,11 +6,6 @@ import { agents } from "../utils/utils";
 import { checkUser } from "../utils/middleware";
 
 const notificationRoutes = express.Router();
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://crm.bettingparadize.com",
-  "https://betting-crm-nextjs-dev.vercel.app",
-];
 notificationRoutes.get("/", checkUser, notificationController.getNotifications);
 notificationRoutes.put(
   "/",
@@ -20,22 +15,18 @@ notificationRoutes.put(
 
 //NOTE:
 // SSE route to stream notifications to agents
-notificationRoutes.get("/sse", (req, res) => {
+notificationRoutes.get("/sse", checkUser, (req, res) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  const token = req.headers.authorization.split(" ")[1];
 
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   // Set the headers for SSE
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-  const token = req.headers.cookie?.split("=")[1];
   const decoded = jwt.verify(token, config.jwtSecret!);
-
   agents.set(decoded.userId, res);
-
   // Clean up when the connection is closed
   req.on("close", () => {
     agents.delete(decoded.userId);
