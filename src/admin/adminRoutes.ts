6431c8a7efce2c83c37a8a283d97717a96029343
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-09-26 17:31:50 +0530

    DELETE:ts files

--- src/admin/adminRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import adminController from "./adminController";
-
-const adminRoutes = express.Router();
-
-adminRoutes.post("/request-otp", adminController.requestOtp);
-adminRoutes.post("/verify-otp", adminController.verifyOtpAndCreateAdmin);
-
-export default adminRoutes;
